d60ab29204815f86959c0ca54a79036ae041af14
zhou <540376482yzb@gmail.com>
2018-12-11 12:02:38 -0800

    folder placeholder

--- helpers/response.helper.js
+++
@@ -1,7 +0,0 @@
-function formattedContent(status, message, payload) {
-    const response = { status, message: message.toUpperCase() }
-    if (payload) { response.payload = payload }
-    return response
-}
-
-module.exports = { formattedContent }
